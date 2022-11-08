e173d413911dfff70a9349547018d03f0340e76c
alextoyinore <aore8030@gmail.com>
2022-11-08 10:28:40 +0100

    Header file

---
+++ simple_shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+
