0776f90622381e7be6f01b374be2c4aacea54e22
iyidaclem <iyidaclem@gmail.com>
2022-11-16 13:46:43 +0100

    Final correction made on the shell

--- shell.c
+++ shell.c
@@ -50,29 +50,6 @@ void set_data(data_shell *datash, char **av)
 	datash->pid = aux_itoa(getpid());
 }
 
-/**
-*main - Entry point into our shell program
-*@argc: argument count
-*@argv: argument to the main function
-*@env:  array of environmental variables
-*Return: Always success (0);
-*/
-
-
-int main(int argc, char **argv)
-{
-	data_shell datash;
-	(void)argc;
-
-	signal(SIGINT, get_sigint);
-	set_data(&datash, argv);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
-}
-
 
 /**
 *mini_shell - helper function for main
@@ -103,3 +80,28 @@ int  mini_shell(char **argv, char **env)
 	return (0);
 
 }
+
+
+/**
+*main - Entry point into our shell program
+*@argc: argument count
+*@argv: argument to the main function
+*Return: Always success (0);
+*/
+
+
+int main(int argc, char **argv)
+{
+	data_shell datash;
+	(void)argc;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, argv);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
+
+
